21a0139b6642d25c1aedd27affb8b463b1445976
Jeffrey Robinson <deathly809@gmail.com>
2016-03-04 17:04:10 -0600

    Support arrays of Value type

--- entry.go
+++ entry.go
@@ -50,12 +50,13 @@ var (
 
 // Types supported
 const (
-	Unknown = Type(iota)
-	Array   = Type(iota)
-	Real    = Type(iota)
-	Integer = Type(iota)
-	Boolean = Type(iota)
-	Text    = Type(iota)
+	Unknown   = Type(iota)
+	Array     = Type(iota)
+	Real      = Type(iota)
+	Integer   = Type(iota)
+	Boolean   = Type(iota)
+	Text      = Type(iota)
+	ValueType = Type(iota)
 )
 
 // Value represents any type of value, this is used
@@ -99,19 +100,25 @@ func WrapUnknown(value interface{}) Value {
 // WrapArray takes an array and returns an array of values
 func WrapArray(value interface{}, theType Type) (result Value, err error) {
 	if value != nil {
-		if t := reflect.TypeOf(value).Kind(); t == reflect.Slice {
-			s := reflect.ValueOf(value)
-			tmp := make([]Value, s.Len())
-			r := (*array)(&tmp)
-			for i := 0; i < s.Len(); i++ {
-				if v, err := conversionFunctions[theType](s.Index(i)); err != nil {
-					result = nil
-					break
-				} else {
-					(*r)[i] = v
+		if t := reflect.TypeOf(value).Kind(); t == reflect.Slice { // is an array
+			if theType == ValueType {
+				tmp := value.([]Value)
+				result = (*array)(&tmp)
+			} else {
+				s := reflect.ValueOf(value)
+				tmp := make([]Value, s.Len())
+				r := (*array)(&tmp)
+				for i := 0; i < s.Len(); i++ {
+					if v, err := conversionFunctions[theType](s.Index(i)); err != nil {
+						result = nil
+						break
+					} else {
+						(*r)[i] = v
+					}
 				}
+				result = r
 			}
-			result = r
+
 		} else {
 			err = fmt.Errorf("Expected array, found %s", t.String())
 		}
